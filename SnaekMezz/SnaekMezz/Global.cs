7e8fe54b62e9a8bce4dfd2a440ad4173c74cebbb
amboiv14 <amboiv14@student.westerdals.no>
2015-10-19 12:49:58 +0200

    Ny commit.

---
+++ SnaekMezz/SnaekMezz/Global.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnaekMezz
+{
+	public static class Global
+	{
+		public static readonly int BoardWidth = Console.WindowWidth;
+		public static readonly int BoardHeight = Console.WindowHeight;
+		public static bool IsGameOver = false;
+		public static bool IsPaused = false;
+		public static bool IsErasable = true;
+	}
+}
